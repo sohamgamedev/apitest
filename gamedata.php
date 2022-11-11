a708e7bff30bcef2a6468fe9f0f4582486d0d4d4
Soham <96002274+sohamgamedev@users.noreply.github.com>
2022-11-11 06:41:32 +0530

    Update gamedata.php

--- gamedata.php
+++ gamedata.php
@@ -1,1 +1,35 @@
+<html>
+    <head><meta http-equiv="Content-Type":"application/json"><head>
 
+<?php
+
+include 'DatabaseConfig.php' ;
+
+$json = file_get_contents('php://input');
+
+$_POST = json_decode($json);
+
+//echo $_POST;  
+
+
+ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+
+ $name = $_POST['name'];
+ $score = $_POST['score']
+
+
+ $Sql_Query = "insert into game_test (Name, score) values ('$name', '$score')";
+
+ if(mysqli_query($con,$Sql_Query)){
+
+ echo 'Data Submit Successfully';
+
+ }
+ else{
+
+ echo 'Try Again';
+
+ }
+ mysqli_close($con);
+?>
+</html>
